d3c3c1db8a183ab124e4b2e1e6ef5c41e4bf4e68
CristinaPineda <cristinadaspineda@yahoo.com.br>
2022-06-02 19:56:47 -0300

    :bangbang: refactor: inicia refatoracao para somente um jogador

--- src/components/jogo/jogos/jogos.jsx
+++ src/components/jogo/jogos/jogos.jsx
@@ -17,6 +17,7 @@ export default function Jogos() {
   const [ptsEquipe, setPtsEquipe] = useState(0);
   const dataLista = question[questionEquipe].dicas;
   const responseQuestion = question[questionEquipe].resposta;
+  // refactor
 
   useEffect(() => {
     setListDicas([dataLista[index]]);
