426b56b661199e012c5072842d325726064814c2
CristinaPineda <cristinadaspineda@yahoo.com.br>
2022-06-05 20:50:59 -0300

    :nail_care: style: inicia estilizacao

--- src/styles/menu/menu.js
+++ src/styles/menu/menu.js
@@ -18,9 +18,21 @@ const MenuStyled = styled.div`
       display: flex;
       justify-content: space-evenly;
       align-items: center;
+      border-top: 1px double darkblue;
+      border-bottom: 1px double darkblue;
+      height: 2.5rem;
       
       a {
         text-decoration: none;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .image_literacao {
+      text-align: center;
+      img {
+        width: 80%;
       }
     }
   }
