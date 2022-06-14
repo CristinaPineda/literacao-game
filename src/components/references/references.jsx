868e56c30aa4fffffa1fd82bd6f76512d64cd91e
CristinaPineda <cristinadaspineda@yahoo.com.br>
2022-06-13 23:24:46 -0300

    :nail_care: styled: estilos das referencias

--- src/components/references/references.jsx
+++ src/components/references/references.jsx
@@ -1,46 +1,49 @@
 import React from 'react';
+import RefersStyled from '../../styles/referencias/refers';
 
 export default function References() {
   return (
-    <>
-      <h2>REFERÊNCIAS BIBLIOGRÁFICAS</h2>
-      <ul>
-        <li>ASSOLINI, F. E. P.; TFOUNI, L. V. Letramento e trabalho pedagógico. Acolhendo a
+    <RefersStyled>
+      <div className="container">
+        <h2>REFERÊNCIAS BIBLIOGRÁFICAS</h2>
+        <ul>
+          <li>ASSOLINI, F. E. P.; TFOUNI, L. V. Letramento e trabalho pedagógico. Acolhendo a
 alfabetização nos países de Língua Portuguesa, [S. l.], v. 1, n. 1, p. 50-72, 2007. DOI:
 10.11606/issn.1980-7686.v1i1p50-72. Disponível em: https://www.revistas.usp.br</li>
-        <li>BRASIL, MEC. Pisa 2018 revela baixo desempenho escolar em Leitura, Matemática e
+          <li>BRASIL, MEC. Pisa 2018 revela baixo desempenho escolar em Leitura, Matemática e
 Ciências no Brasil. 2019. Disponível em: http://portal.mec.gov.br. Acesso em: 09 maio 2022.</li>
-        <li>BRITO, D. S. de. A importância da leitura na formação social do indivíduo. Revela, Guarujá,
+          <li>BRITO, D. S. de. A importância da leitura na formação social do indivíduo. Revela, Guarujá,
 v. 1, n. 8, p. 1-35, jun. 2010. Semestral. Disponível em: http://www.fals.com.br/revela. Acesso em:
 09 maio 2022.</li>
-        <li>CEREJA, W. R. Uma proposta dialógica de ensino de literatura no Ensino Médio. 2004. 330
+          <li>CEREJA, W. R. Uma proposta dialógica de ensino de literatura no Ensino Médio. 2004. 330
 f. Tese (Doutorado) - Curso de Linguística Aplicada e Estudos da Linguagem, Pontifícia
 Universidade Católica, São Paulo, 2004. Disponível em: http://d1wqtxts1xzle7.cloudfront.net.
 Acesso em: 26 abr. 2022.</li>
-        <li>DOHME, V. Atividades lúdicas na educação: o caminho de tijolos amarelos do aprendizado.
+          <li>DOHME, V. Atividades lúdicas na educação: o caminho de tijolos amarelos do aprendizado.
 Petrópolis, Rio de Janeiro: Vozes, 2008.</li>
-        <li>DUSI, M. L. H. M.; NEVES, M. M. B. da J.; ANTONY, S. Abordagem gestáltica e
+          <li>DUSI, M. L. H. M.; NEVES, M. M. B. da J.; ANTONY, S. Abordagem gestáltica e
 psicopedagogia: um olhar compreensivo para a totalidade criança-escola. Ribeirão Preto:
 Paidéia, 2006.</li>
-        <li>EAGLETON, T. Teoria da Literatura: uma introdução. Tradução de Waltensir Dutra. São
+          <li>EAGLETON, T. Teoria da Literatura: uma introdução. Tradução de Waltensir Dutra. São
 Paulo: Martins Fontes, 2003.</li>
-        <li>FERRAZ, A. P. C. M.; BELHOT, R. V. Taxonomia de Bloom: revisão teórica e apresentação
+          <li>FERRAZ, A. P. C. M.; BELHOT, R. V. Taxonomia de Bloom: revisão teórica e apresentação
 das adequações do instrumento para definição de objetivos instrucionais. Gestão & Produção,
 São Carlos, 2010.</li>
-        <li>FOLHA DE SÃO PAULO. Estudo comprova que criança que lê mais escreve melhor. Barueri,
+          <li>FOLHA DE SÃO PAULO. Estudo comprova que criança que lê mais escreve melhor. Barueri,
 São Paulo, 2010. Disponível em: www1.folha.uol.com.br. Acesso em 12 abr. 2022</li>
-        <li>FREIRE, P. A importância do ato de ler - em três artigos que se completam. São Paulo: Cortez
+          <li>FREIRE, P. A importância do ato de ler - em três artigos que se completam. São Paulo: Cortez
 Editora & Autores Associados, 1991.</li>
-        <li>LUCKESI, C. C. Educação, ludicidade e prevenção das neuroses futuras: uma proposta
+          <li>LUCKESI, C. C. Educação, ludicidade e prevenção das neuroses futuras: uma proposta
 pedagógica a partir da biossíntese. In Educação e Ludicidade, Coletânea Ludopedagogia Ensaios
 01, organizada por Cipriano Carlos Luckesi, publicada pelo GEPEL, Programa de Pós-Graduação
 em Educação, FACED/UFBA, 2000.</li>
-        <li>MORIN, E. Complexidade e transdisciplinaridade: a reforma da universidade e do Ensino
+          <li>MORIN, E. Complexidade e transdisciplinaridade: a reforma da universidade e do Ensino
 Fundamental. Natal: Editora da UFRN, 1999.</li>
-        <li>MIRANDA, S. de. No fascínio do jogo, a alegria de aprender. Linhas Críticas. Disponível em:
+          <li>MIRANDA, S. de. No fascínio do jogo, a alegria de aprender. Linhas Críticas. Disponível em:
 https://doi.org. Acesso em: 15 mai. 2022.</li>
-      </ul>
-    </>
+        </ul>
+      </div>
+    </RefersStyled>
   );
 }
 
---
+++ src/styles/referencias/refers.js
@@ -0,0 +1,43 @@
+
+import styled from 'styled-components';
+import cartas from '../../images/cartas.png';
+
+const RefersStyled = styled.div`
+  background-image: url(${cartas});
+  background-repeat: repeat-x;
+  background-position: center top;
+  background-size: contain;
+  width: 99%;
+  margin: auto;
+  color: 	#1C1C1C;
+  text-align: center;
+  
+  .container {
+    margin: 0.5rem 0;
+    padding: 0.5rem 3.5rem;
+    line-height: 2rem;
+    background-color: rgba(220,220,220,.8);
+    color: 	#1C1C1C;
+    text-align: left;
+
+    ul {
+      list-style-type: square;
+    }
+    h2 {
+      text-align: center;
+      color: darkred;
+    }
+  }
+
+
+  @media (max-width: 768px) {
+    background-repeat: repeat-y;
+
+    .container {
+    padding: 0.5rem 1.5rem;
+    }
+  }
+
+  `;
+
+export default RefersStyled;
